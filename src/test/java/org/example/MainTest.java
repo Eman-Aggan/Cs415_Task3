af78285db050b982158c5bbf1cf5f804a47fa4ee
Eman-Aggan <eman.aggan123@gmail.com>
2025-10-17 16:58:28 +0300

    false multiplication

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,27 +1,21 @@
 package org.example;
 
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-@Test
-    public void testSum(){
-        int result = Main.sum(5,3);
-        assertEquals(8 , result ,"5 + 3 should equal 8");
 
-
+    @Test
+    void testSum() {
+        int result = Main.sum(5, 3);
+        assertEquals(8, result, "5 + 3 should equal 8");
     }
 
-
-
-@Test
-    public void testMultiply(){
-        int result = Main.multiply(5,3);
-        assertEquals(15 , result ,"5 + 3 should equal 15");
-
-
+    @Test
+    void testMultiply() {
+        int result = Main.multiply(5, 3);
+        assertEquals(15, result, "5 * 3 should equal 15");
     }
-
 }
+
 
